f07d1cd9bbfe05b0466ff497304185ba59c01896
Sascha Heilmeier <sheilmeier@gmail.com>
2016-02-27 12:23:00 +0100

    Change version to 0.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,15 +8,11 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '0.2.0',
+    'version' => '0.3.0',
     'constraints' => array(
 		'depends' => array (
 			'typo3' => '6.2.0-7.6.99',
 			'php' => '5.5.0-7.0.99'
 		),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
     ),
 );
