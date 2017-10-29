432f87d33443af8c12416a1af45b483b6d81ed9c
Sascha <sheilmeier@gmail.com>
2017-10-29 12:04:38 +0100

    set version to 0.3.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_company' => '',
 	'state' => 'beta',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.3.3',
+	'version' => '0.3.4',
 	'constraints' =>
 	array (
 		'depends' =>
